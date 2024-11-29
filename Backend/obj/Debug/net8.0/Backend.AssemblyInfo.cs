ee3f20bf3775705d66b892f84f63695d2408fde9
Ant <Drakarta3@gmail.com>
2024-11-29 15:16:04 +0100

    changed backend file structure

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+174e23a550847fb9b542af19b7ac3ab5b9477ab7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf1e0c400c63559397cf506b4adde51baf0d327c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
