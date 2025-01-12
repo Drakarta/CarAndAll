895523b7d39bfb224d2cdda40818f1b8b1c92439
Rody <rody.van.dijk@gmail.com>
2025-01-12 18:51:37 +0100

    Checking if input is null or empty or filled to make sure no empty data in verhuuraanvraag

--- Backend/Controllers/VerhuurAanvraagController.cs
+++ Backend/Controllers/VerhuurAanvraagController.cs
@@ -14,6 +14,7 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
 
 
 namespace Backend.Controllers
@@ -37,6 +38,23 @@ namespace Backend.Controllers
         {
             try
             {
+                 if (model.Bestemming.IsNullOrEmpty())
+                {
+                    var errorDetails = new {
+                        message = "The 'Bestemming'input is empty",
+                        statusCode = 400
+                    };
+                    return BadRequest(errorDetails);
+                }
+                if (model.Kilometers == 0)
+                {
+                    var errorDetails = new {
+                        message = "The 'Kilometers' input is empty",
+                        statusCode = 400
+                    };
+                    return BadRequest(errorDetails);
+                }
+                
                 var accountEmail = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email);
                 var account_id = await _context.Account
                     .Where(a => a.Email == accountEmail.Value)
