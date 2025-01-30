9100f0137f29e52e75c86c5a8cb9dc7a04f384de
Anthony <drakarta3@gmail.com>
2025-01-30 20:44:51 +0100

    index.tsx loginRegister.tsx privacy.tsx privacyEdit.tsx comments

--- Frontend/src/pages/index.tsx
+++ Frontend/src/pages/index.tsx
@@ -10,19 +10,24 @@ import porsche from "../assets/porsche2.png";
 import "../styles/homepage.css";
 
 export default function Index() {
+  // Array of images to display in the courasel
   const images = [audi, bmw, koenigsegg, miata, porsche];
+  // State to store the current image number
   const [imageNr, setImageNr] = useState<number>(0);
+  // Function to change the image in the courasel
   function changeImage(incriment: number) {
     setImageNr((imageNr + incriment + images.length) % images.length);
   }
 
   return (
     <>
+      {/* Banner text */}
       <div className="page-center banner-text">
         <h1>CarAndAll</h1>
         <p>Car, caravans and campers</p>
         {/* <button>Rent now!</button> */}
       </div>
+      {/* Courasel to display the images */}
       <div className="image-courasel page-center">
         <img className="image-in-courasel" src={images[imageNr]} alt="Car images" />
         <div className="courasel-buttons">
--- Frontend/src/pages/loginRegister.tsx
+++ Frontend/src/pages/loginRegister.tsx
@@ -6,9 +6,12 @@ import RegisterForm from "../components/registerForm";
 import { useTokenStore } from "../stores";
 
 export default function LoginRegister() {
+  // State to determine if the user is registering or logging in
   const [register, setRegister] = useState(false);
+  // State to determine if the user is a business or not
   const [bussiness, setBussiness] = useState(false);
   const token = useTokenStore((state) => state.token);
+  // If the user is already logged in, redirect to the homepage
   if (token) {
     window.location.href = "/";
   }
@@ -17,10 +20,12 @@ export default function LoginRegister() {
     <>
       <div className={"page-center"}>
         <div className={"auth-container"}>
+          {/** If the user is registering, show the register form, otherwise show the login form */}
           {register ? <RegisterForm bussiness={bussiness} /> : <LoginForm />}
           <button className={"button"} onClick={() => setRegister(!register)}>
             {register ? "Already have an account?" : "Don't have an account?"}
           </button>
+          {/** If the user is registering, show the button to switch between business and private renter */}
           <button className={"button"} onClick={() => {setBussiness(!bussiness), setRegister(true)}}>
             {bussiness ? "Register as a private renter" : "Register as a business"}
           </button>
